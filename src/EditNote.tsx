9192e59ce88742d366834b7fff911ea436e28ee9
Jingcheng Wang <jingwang49@gmail.com>
2024-02-17 15:38:34 +0800

    edit function complete

--- src/EditNote.tsx
+++ src/EditNote.tsx
@@ -8,12 +8,18 @@ type EditNoteProps = {
     availableTags: Tag[]
 }
 
-export function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps ) {
+export function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps) {
     const note = useNote()
     return (
         <>
             <h1 className="mb-4">Edit Note</h1>
-            <NoteForm onSubmit={data => onSubmit(note.id, data)} onAddTag={onAddTag} availableTags={availableTags} />
+            <NoteForm
+                title={note.title}
+                markdown={note.markdown}
+                tags={note.tags}
+                onSubmit={data => onSubmit(note.id, data)}
+                onAddTag={onAddTag}
+                availableTags={availableTags} />
         </>
     )
 }

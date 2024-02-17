5daa7c408709ae123d17fccded7f3405d6c6ca32
Jingcheng Wang <jingwang49@gmail.com>
2024-02-17 13:07:07 +0800

    create new note and tags complete

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,3 +1,17 @@
-export function NewNote() {
-    return <h1>Hi</h1>
+import { NoteData, Tag } from "./App"
+import { NoteForm } from "./NoteForm"
+
+type NewNoteProps = {
+    onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
+}
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps ) {
+    return (
+        <>
+            <h1 className="mb-4">New Note</h1>
+            <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
+        </>
+    )
 }
